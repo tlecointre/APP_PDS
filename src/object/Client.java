778114d44b55a4b8a43cd2e9fd37b3ddb10987cb
Nadia Randria <Nadia Randria@192.168.1.55>
2016-04-03 14:57:00 +0200

    Implémentation de toutes les méthodes

--- src/object/Client.java
+++ src/object/Client.java
@@ -8,9 +8,10 @@ package object;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-//import java.sql.Statement;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import pds_model.PdsDatabase;
@@ -21,8 +22,10 @@ import pds_model.PdsDatabase;
  * @author hubanato
  */
 public class Client extends Person {
-    
-    int generatedId;
+
+    private int id_adr;
+    private int id_person;
+
     /**
      * Constructor This method is used to create a new client
      *
@@ -75,29 +78,15 @@ public class Client extends Person {
 
     /**
      *
-     */
-    @Override
-    public void Consult() {
-
-    }
-
-    /**
-     *
      *
      */
     @Override
     public void CreatePerson() {
         try {
             Connection connexion = PdsDatabase.getConnection();
-            
-            String requeteAddress = "SET SERVEROUTPUT ON\n" +
-                                    "BEGIN\n" +
-                                    "  INSERT INTO ADDRESS(NBER,STREET,ADDITIONAL,ZIP_CODE,CITY,COUNTRY) VALUES (?,?,?,?,?,?)\n" +
-                                    "  RETURNING id_adr INTO ?;\n" +
-                                    "  COMMIT;\n" +
-                                    "END";
-                    //+ "INSERT INTO ADDRESS(NBER,STREET,ADDITIONAL,ZIP_CODE,CITY,COUNTRY) "
-                    //+ "VALUES (?,?,?,?,?,?)";
+
+            //This query is used to insert first into the table "ADDRESS"
+            String requeteAddress = "INSERT INTO ADDRESS(NBER,STREET,ADDITIONAL,ZIP_CODE,CITY,COUNTRY) VALUES (?,?,?,?,?,?)";
             //PreparedStatement ordre = connexion.prepareStatement(requeteAddress, Statement.RETURN_GENERATED_KEYS);
             PreparedStatement ordre = connexion.prepareStatement(requeteAddress);
 
@@ -107,28 +96,27 @@ public class Client extends Person {
             ordre.setInt(4, cp);
             ordre.setString(5, city);
             ordre.setString(6, country);
-            ordre.setInt(7, generatedId);
 
             ordre.executeUpdate();
             System.out.println(requeteAddress);
-            
-            
-            /*ResultSet rs = ordre.getGeneratedKeys();
-            int generatedId = 0;
-            if(rs.next()) {            
-                //Get the last id
-                generatedId = rs.getInt(1);
-                System.out.println(generatedId);
-            }*/
-            System.out.println(generatedId);
-            
+
+            ////This query is used to select the max(id) into the table "ADDRESS"
+            String requeteIdMax = "SELECT MAX(id_adr) INTO idMax FROM ADDRESS";
+            Statement IdRecup = connexion.createStatement();
+            ResultSet rs = IdRecup.executeQuery(requeteIdMax);
+
+            while (rs.next()) {
+                id_adr = rs.getInt("idMax");
+            }
+
             ordre.close();
-            
+
+            //This query is used to insert after into the table "PERSON"
             String requeteClient = "INSERT INTO PERSON(ID_ADR,CIVILITY,FIRST_NAME,NAME,SEX,BIRTH_DATE,BIRTH_PLACE,NATIONALITY,PHONE_HOME,PHONE_MOBIL,EMAIL,JOB,PHONE_BUSINESS) "
                     + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement ordre2 = connexion.prepareStatement(requeteClient);
 
-            ordre2.setInt(1, generatedId);
+            ordre2.setInt(1, id_adr);
             ordre2.setString(2, civility);
             ordre2.setString(3, firstName);
             ordre2.setString(4, name);
@@ -155,11 +143,77 @@ public class Client extends Person {
     }
 
     /**
+     * Returns a client
+     *
+     * @param id : ID client
+     * @return client found
+     * @throws SQLException
+     */
+    public static Client getById(int id) throws SQLException {
+        Connection connexion = PdsDatabase.getConnection();
+        String sql = "SELECT * FROM PERSON, ADDRESS WHERE ID_PERS = ? AND PERSON.ID_ADR = ADDRESS.ID_ADR";
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setInt(1, id);
+        ResultSet rs = ordre.executeQuery();
+        if (rs.next()) {
+            return new Client(rs.getString("CIVILITY"), rs.getString("NAME"), rs.getString("FIRST_NAME"),
+                    rs.getDate("BIRTH_DATE"), rs.getString("BIRTH_PLACE"), rs.getString("SEX"),
+                    rs.getString("NATIONALITY"), rs.getInt("NBER"), rs.getString("STREET"),
+                    rs.getString("ADDITIONAL"), rs.getInt("ZIP_CODE"), rs.getString("CITY"),
+                    rs.getString("COUNTRY"), rs.getInt("PHONE_MOBIL"), rs.getInt("PHONE_HOME"),
+                    rs.getInt("PHONE_BUSINESS"), rs.getString("EMAIL"), rs.getString("JOB"));
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a client
+     *
+     * @param name : client name
+     * @param firstName : client first name
+     * @return client found
+     * @throws SQLException
+     */
+    public static ArrayList<Client> getByName(String name, String firstName) throws SQLException {
+        Connection connexion = PdsDatabase.getConnection();
+
+        String sql = "SELECT * FROM PERSON, ADDRESS WHERE NAME = ? AND FIRST_NAME = ? AND PERSON.ID_ADR = ADDRESS.ID_ADR";
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setString(1, name);
+        ordre.setString(2, firstName);
+        ResultSet rs = ordre.executeQuery();
+        ArrayList<Client> clients = new ArrayList<Client>();
+        while (rs.next()) {
+            clients.add(new Client(rs.getString("CIVILITY"), rs.getString("NAME"), rs.getString("FIRST_NAME"),
+                    rs.getDate("BIRTH_DATE"), rs.getString("BIRTH_PLACE"), rs.getString("SEX"),
+                    rs.getString("NATIONALITY"), rs.getInt("NBER"), rs.getString("STREET"),
+                    rs.getString("ADDITIONAL"), rs.getInt("ZIP_CODE"), rs.getString("CITY"),
+                    rs.getString("COUNTRY"), rs.getInt("PHONE_MOBIL"), rs.getInt("PHONE_HOME"),
+                    rs.getInt("PHONE_BUSINESS"), rs.getString("EMAIL"), rs.getString("JOB")));
+
+        }
+        return clients;
+    }
+
+    /**
      *
      */
     @Override
     public void UpdatePerson() {
-
+        try {
+            Connection connexion = PdsDatabase.getConnection();
+            String sql = "UPDATE PERSON SET name = ? , firstName = ? WHERE id = ?";
+            PreparedStatement ordre = connexion.prepareStatement(sql);
+            ordre.setString(1, name);
+            ordre.setString(2, firstName);
+            ordre.setInt(3, id_person);
+            ordre.executeUpdate();
+            ordre.close();
+            connexion.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -168,7 +222,19 @@ public class Client extends Person {
      */
     @Override
     public void DeletePerson(int id) {
+        try {
+            Connection connexion = PdsDatabase.getConnection();
 
+            String deleteQuery = "DELETE FROM PERSON WHERE id = ?";
+            PreparedStatement ordre = connexion.prepareStatement(deleteQuery);
+            ordre.setInt(1, id);
+            ordre.executeUpdate();
+            ordre.close();
+            connexion.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
