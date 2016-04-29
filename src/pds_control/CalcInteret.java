e410f4eca35bd422dda4d6470e96025bfc98df56
Nadia Randria <Nadia Randria@192.168.1.55>
2016-04-29 17:08:04 +0200

    This clss is used to calculate the interest rate for the simulation

---
+++ src/pds_control/CalcInteret.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pds_control;
+
+/**
+ * This class is used to calcultate the intersest rate
+ * @author Nadia Randria
+ */
+public class CalcInteret {
+    
+}
