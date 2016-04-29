47f59fab847ef7816f68441b8df6036844eb923d
Nadia Randria <Nadia Randria@192.168.1.55>
2016-04-29 22:09:31 +0200

    Add the method CalculerTaux(), CalcInteretForm() and actionPerformed()

--- src/pds_control/CalcInteret.java
+++ src/pds_control/CalcInteret.java
@@ -5,10 +5,43 @@
  */
 package pds_control;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
 /**
  * This class is used to calcultate the interest rate
  * @author Nadia Randria
  */
-public class CalcInteret {
+public class CalcInteret implements ActionListener{
     
+    private JTextField emprunt;
+    private JTextField duree;
+    private JTextField apport;
+    private JTextField taux;
+    private JButton calculer;
+
+    public CalcInteret(JTextField emprunt, JTextField duree, JTextField apport, JTextField taux, JButton calculer) {
+        this.emprunt = emprunt;
+        this.duree = duree;
+        this.apport = apport;
+        this.taux = taux;
+        this.calculer = calculer;
+    }
+
+    /**
+     * This method permit to calculate the interest rate
+     * @return 
+     */
+    public float CalculerTaux(){
+        return 0;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        Object source = evt.getSource();
+
+        if (source == calculer) {}
+    }
 }
