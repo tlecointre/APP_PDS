d624107980ab98cbac8099381ea678bdbf53e52a
Baptiste <Baptiste@DESKTOP-TS0A63V>
2016-04-29 16:56:53 +0200

    add JSON

---
+++ src/json/DecodeJSON.java
@@ -0,0 +1,16 @@
+
+package JSON;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Baptiste
+ */
+public class DecodeJSON {
+    
+   
+}
