56cbf5344155d8f313da6312f393ce961a31edfe
Baptiste <>
2016-04-29 16:58:29 +0200

    add JSON

---
+++ src/pds_json/DecodeJSON.java
@@ -0,0 +1,16 @@
+
+package pds_json;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Baptiste
+ */
+public class DecodeJSON {
+    
+   
+}
