70ff4a6bc1300ec13c872614331f3cda00ba8362
Tony <>
2016-05-02 14:28:27 +0200

    Changement des variables en Anglais

--- src/pds_ihm/GraphForm.java
+++ src/pds_ihm/GraphForm.java
@@ -1,11 +1,8 @@
 package pds_ihm;
 
-import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 
 /**
@@ -22,14 +19,14 @@ public class GraphForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        graphBouton = new javax.swing.JButton();
+        graphButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        graphBouton.setText("Afficher Graph");
-        graphBouton.addActionListener(new java.awt.event.ActionListener() {
+        graphButton.setText("Afficher Graph");
+        graphButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                graphBoutonActionPerformed(evt);
+                graphButtonActionPerformed(evt);
             }
         });
 
@@ -39,35 +36,35 @@ public class GraphForm extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(81, 81, 81)
-                .addComponent(graphBouton)
+                .addComponent(graphButton)
                 .addContainerGap(184, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(116, 116, 116)
-                .addComponent(graphBouton)
+                .addComponent(graphButton)
                 .addContainerGap(155, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void graphBoutonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_graphBoutonActionPerformed
-        int resteAPayer = 100;
-        int totalAPayer = 1000;
-        int dejaPaye = 900;
+    private void graphButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_graphButtonActionPerformed
+        int leftToPay = 100;
+        int totalToPay = 1000;
+        int paid = 900;
         
         DefaultPieDataset test = new DefaultPieDataset();
-        test.setValue("Reste à payer: "+resteAPayer+"E", new Integer(resteAPayer));
-        test.setValue("Payé: "+dejaPaye+"E", new Integer(dejaPaye));
-        test.setValue("Total: "+totalAPayer+"E", new Integer(0));
+        test.setValue("Reste à payer: "+leftToPay+"E", new Integer(leftToPay));
+        test.setValue("Payé: "+paid+"E", new Integer(paid));
+        test.setValue("Total: "+totalToPay+"E", new Integer(0));
         JFreeChart chart = ChartFactory.createPieChart("Prêt", test, true, true, true);
         
         DefaultPieDataset test1 = new DefaultPieDataset();
-        test1.setValue("Reste à payer: "+resteAPayer+"E", new Integer(resteAPayer));
-        test1.setValue("Payé: "+dejaPaye+"E", new Integer(dejaPaye));
-        test1.setValue("Total: "+totalAPayer+"E", new Integer(0));
+        test1.setValue("Reste à payer: "+leftToPay+"E", new Integer(leftToPay));
+        test1.setValue("Payé: "+paid+"E", new Integer(paid));
+        test1.setValue("Total: "+totalToPay+"E", new Integer(0));
         JFreeChart chart1 = ChartFactory.createPieChart("Prêt", test1, true, true, true);
         
         ChartPanel pan = new ChartPanel(chart, false);
@@ -81,7 +78,7 @@ public class GraphForm extends javax.swing.JFrame {
         this.setVisible(true);
         this.setSize(1000,1000);
         
-    }//GEN-LAST:event_graphBoutonActionPerformed
+    }//GEN-LAST:event_graphButtonActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -92,6 +89,6 @@ public class GraphForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton graphBouton;
+    private javax.swing.JButton graphButton;
     // End of variables declaration//GEN-END:variables
 }
