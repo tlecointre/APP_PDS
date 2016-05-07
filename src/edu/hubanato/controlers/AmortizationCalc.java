a245007da8f040e27b51e9cff701bf2cf29fff6e
Tony <Tony@mbp-de-tony>
2016-05-07 20:13:44 +0200

    AmortizationCalc updated

--- src/edu/hubanato/controlers/AmortizationCalc.java
+++ src/edu/hubanato/controlers/AmortizationCalc.java
@@ -7,37 +7,39 @@ public class AmortizationCalc {
 
     public static void main(String[] args) {
         double amount, taux;
-        int nbyear;
-        amount = 10000;
-        taux = 10;
-        nbyear = 2;
+        int nbYear;
+        amount = 10000; //à modifier avec la BDD
+        taux = 10; //à modifier avec la BDD
+        nbYear = 2; //à modifier avec la BDD
         
         AmortizationScheduleForm t1 = new AmortizationScheduleForm();
         t1.setVisible(true);
-        calAmort(amount, taux, nbyear,t1);
+        calAmort(amount, taux, nbYear,t1);
 
     }
 
-    public static void calAmort(double amount, double iy, int ny, AmortizationScheduleForm t1) {
+    public static void calAmort(double amount, double interest, int nbYear, AmortizationScheduleForm t1) {
         double newAmount;
-        double im = (iy / 12) / 100;
-        int nbMonth = ny * 12;
+        double im = (interest / 12) / 100;
+        int nbMonth = nbYear * 12;
         double monthlyPayment, interestPaid, principalPaid;
         int i;
         DecimalFormat df = new DecimalFormat("########.000"); 
+        
+        //Formule de calcul des mensualités
         monthlyPayment = amount * im * Math.pow(1 + im, (double) nbMonth) / (Math.pow(1 + im, (double) nbMonth) - 1);
-        //print amortization schedule for all months except the last month
+       
         for (i = 1; i < nbMonth; i++) {
-            interestPaid = amount * im;//interest paid
-            principalPaid = monthlyPayment - interestPaid; //princial paid
-            newAmount = amount - principalPaid; //new balance                 
+            interestPaid = amount * im;//Intérêt du mois
+            principalPaid = monthlyPayment - interestPaid; //Mensualité sans intérêt
+            newAmount = amount - principalPaid; //Restant à payer               
             t1.getTable().setValueAt(i, i-1, 0);
             t1.getTable().setValueAt(df.format(principalPaid), i-1, 1);
             t1.getTable().setValueAt(df.format(interestPaid), i-1, 2);
             t1.getTable().setValueAt(df.format(newAmount), i-1, 3);
             t1.getTable().setValueAt(0, i-1, 4);
             t1.getTable().setValueAt(df.format(monthlyPayment), i-1, 5);
-            amount = newAmount;  //update old balance
+            amount = newAmount;  //On met le nouveau montant comme montant principal
         }
         //last month
         principalPaid = amount;
