1ea52a63447c2246f994e8569e8cddaa7edb9d0f
Tom <Tom@192.168.1.34>
2016-05-15 16:40:33 +0200

    Addition of idSimulation in constructor / Addition of updateSimulation method

--- src/edu/hubanato/entities/Simulation.java
+++ src/edu/hubanato/entities/Simulation.java
@@ -2,6 +2,8 @@ package edu.hubanato.entities;
 
 import edu.hubanato.models.PdsDatabase;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,12 +15,17 @@ public class Simulation {
     private double rate;
     private String loanType;
     
-    public Simulation(int idClient, int amount, int duration, double rate, String loanType) {
+    public Simulation(int idSimulation, int idClient, int amount, int duration, double rate, String loanType) {
+        this.idSimulation = idSimulation;
         this.idClient = idClient;
         this.amount = amount;
         this.duration = duration;
         this.rate = rate;
         this.loanType = loanType;
+    }
+
+    public int getIdSimulation() {
+        return idSimulation;
     }
     
     public void createSimulation() throws SQLException {
@@ -47,20 +54,47 @@ public class Simulation {
         connection.close();
     }
     
-    public static Simulation getByClient(int idClient) throws SQLException {
-        Connection connexion = PdsDatabase.getConnection();
+    public static List<Simulation> getByClient(int idClient) throws SQLException {
+        Connection connection = PdsDatabase.getConnection();
         String sql = "SELECT * FROM SIMULATION s, TYPES t WHERE ID_CUSTOMER = ? AND s.ID_TYPES = t.ID_TYPES";
-        PreparedStatement ordre = connexion.prepareStatement(sql);
+        PreparedStatement ordre = connection.prepareStatement(sql);
         ordre.setInt(1, idClient);
         ResultSet rs = ordre.executeQuery();
-        if (rs.next()) {
-            return new Simulation(rs.getInt("ID_CUSTOMER"), rs.getInt("AMOUNT"), rs.getInt("DURATION"),
-                    rs.getDouble("RATE_FINAL"), rs.getString("TITLE"));
-        } else {
-            return null;
+        List<Simulation> simulations = new ArrayList<Simulation>();
+        while (rs.next()) {
+            simulations.add(new Simulation(rs.getInt("ID_SIMULATION"), rs.getInt("ID_CUSTOMER"), rs.getInt("AMOUNT"), rs.getInt("DURATION"),
+                    rs.getDouble("RATE_FINAL"), rs.getString("TITLE")));
         }
+        return simulations;
     }
+    
+    /**
+     *Update a simulation in database
+     */
+    public void updateSimulation() throws SQLException {
+        Connection connection = PdsDatabase.getConnection();
 
+        int idLoanType;
+        String sql = "SELECT ID_TYPES AS idLoanType FROM TYPES WHERE TITLE = ?";
+        PreparedStatement o = connection.prepareStatement(sql);
+        o.setString(1, loanType);
+        ResultSet rs = o.executeQuery();
+        rs.next();
+        idLoanType = rs.getInt("idLoanType");
+
+        String queryUSimulation = "UPDATE SIMULATION SET AMOUNT = ? , DURATION = ?, ID_TYPES = ?, RATE_FINAL = ? "
+                    + "WHERE ID_SIMULATION = ?";
+        PreparedStatement ordre = connection.prepareStatement(queryUSimulation);
+        ordre.setInt(1, amount);
+        ordre.setInt(2, duration);
+        ordre.setInt(3, idLoanType);
+        ordre.setDouble(4, rate);
+        ordre.setInt(5,idSimulation);
+        ordre.executeUpdate();
+        ordre.close();
+        connection.close();
+    }
+    
     public int getAmount() {
         return amount;
     }
