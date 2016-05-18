be483ab45f903a6138b244e11a98e22fd03e3951
Tom <Tom@10.10.14.147>
2016-05-18 17:52:23 +0200

    Addition of a class muti-threads class

---
+++ src/edu/hubanato/server/TCPServerThread.java
@@ -0,0 +1,33 @@
+package edu.hubanato.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.*;
+
+public abstract class TCPServerThread {
+    
+    private ServerSocket srv;
+
+    public TCPServerThread(int port) throws IOException {
+        this.srv = new ServerSocket(port);
+    }
+    
+    public void go() throws IOException {
+
+        ThreadGroup thg = new ThreadGroup("server"){
+            public void uncaughtException(Thread t, Throwable e) {
+                Logger.global.log(Level.SEVERE,t.toString(), e);
+            }
+        };
+       
+        while (true) {
+            Socket sck = srv.accept();
+            new Thread(thg, launchSession(sck)).start();
+        }
+        
+    }
+    
+    public abstract Runnable launchSession(Socket sck);
+    
+    
+}
